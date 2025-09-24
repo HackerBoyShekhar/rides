993dd3c4758eb6fcc92e21ff58b1b94c5fb7245f
Shekhar Singh <156294819+HackerBoyShekhar@users.noreply.github.com>
2025-09-24 22:00:14 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_HiXtxhk0J', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '13p6aqr5vj2bnalpbu6ijjqcal', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_HiXtxhK0j', // ✅ this is correct
+        userPoolClientId: 'NEW_APP_CLIENT_ID_WITHOUT_SECRET', // 🔴 update this
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // keep empty until API Gateway is set up
     }
 };
+
